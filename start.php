0f5c70add30bb3083cc882aa9efaf94746086f0d
Ben Werdmuller <ben@benwerd.com>
2013-01-20 22:28:56 -0800

    Bonita now uses autoloading instead of direct includes

--- start.php
+++ start.php
@@ -13,14 +13,11 @@
 	
 	*/
 	
+	// Bonita uses autoloading
 		
-	// Load required classes
-		require_once(dirname(__FILE__) . '/includes/classes/bon.class.php');
-		require_once(dirname(__FILE__) . '/includes/classes/bontemp.class.php');
-		require_once(dirname(__FILE__) . '/includes/classes/bonform.class.php');
-		
-	// Load required interfaces
-		require_once(dirname(__FILE__) . '/includes/interfaces/bondrawable.interface.php');
+		spl_autoload_register(function($class) {
+		    require_once(dirname(__FILE__) . '/includes/classes/'.$class.'.class.php');
+		});
 		
 	// Set Bonita base path to the directory this file is in
 		Bon::$path = dirname(__FILE__);
